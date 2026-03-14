085b241edd478417e059751487fcb138ef221aca
Kazuhito Nishi <kngoofy@outlook.com>
2026-03-14 11:46:01 +0900

    update
    	new file:   cmd/tips/excelize/sample1/main.go
    	new file:   cmd/tips/excelize/sample2/main.go
    	new file:   cmd/tips/excelize/sample3/connector_auto.xlsx
    	new file:   cmd/tips/excelize/sample3/main.go
    	new file:   cmd/tips/excelize/sample4/image.gif
    	new file:   cmd/tips/excelize/sample4/image.jpg
    	new file:   cmd/tips/excelize/sample4/image.png
    	new file:   cmd/tips/excelize/sample4/main.go
    	deleted:    cmd/tips/main.go
    	modified:   tips.txt

---
+++ cmd/tips/excelize/sample4/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+	f, err := excelize.OpenFile("Book1.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		// Close the spreadsheet.
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	// Insert a picture.
+	if err := f.AddPicture("Sheet1", "A2", "image.png", nil); err != nil {
+		fmt.Println(err)
+	}
+	// Insert a picture to worksheet with scaling.
+	if err := f.AddPicture("Sheet1", "D2", "image.jpg",
+		&excelize.GraphicOptions{ScaleX: 0.5, ScaleY: 0.5}); err != nil {
+		fmt.Println(err)
+	}
+	// Insert a picture offset in the cell with printing support.
+	enable, disable := true, false
+	if err := f.AddPicture("Sheet1", "H2", "image.gif",
+		&excelize.GraphicOptions{
+			PrintObject:     &enable,
+			LockAspectRatio: false,
+			OffsetX:         15,
+			OffsetY:         10,
+			Locked:          &disable,
+		}); err != nil {
+		fmt.Println(err)
+	}
+	// Save the spreadsheet with the origin path.
+	if err = f.Save(); err != nil {
+		fmt.Println(err)
+	}
+}
